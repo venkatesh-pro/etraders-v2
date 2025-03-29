1eed5745ca8cb07a5f92856ddde45184b8e23896
venkatesh k <venkateshk@venkateshs-MacBook-Pro.local>
2025-03-29 11:23:58 +0530

    completed

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -8,11 +8,12 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Navbar = () => {
   const [isShowMobileMenu, setIsShowMobileMenu] = useState(false);
-  const mobileMenuRef = useRef(null);
+  const mobileMenuRef = useRef<gsap.core.Timeline | null>(null);
+  const mobileMenuTl = useRef(null);
 
   useGSAP(() => {
+    // Navbar scroll animation
     gsap.set("#homepage-navbar", { yPercent: 0 });
-
     const showAnim = gsap.to("#homepage-navbar", {
       yPercent: -100,
       duration: 0.5,
@@ -30,21 +31,35 @@ const Navbar = () => {
         }
       },
     });
+  }, []);
 
-    if (mobileMenuRef.current) {
-      const tl = gsap.timeline();
+  // Initialize timeline only once
+  useEffect(() => {
+    if (mobileMenuRef.current && mobileMenuTl.current === null) {
+      mobileMenuTl.current = gsap.timeline({ paused: true });
+      mobileMenuTl.current.to("#logo", { opacity: 0, duration: 0.3 });
+      mobileMenuTl.current.to("#hamburger", { opacity: 0, duration: 0.3 }, "<"); // Run at the same time
 
-      tl.to(".isShowMobileMenu", {
-        height: "100%",
-        position: "fixed",
-        display: "block",
-        opacity: 1,
-        duration: 1,
-      });
+      mobileMenuTl.current.to(
+        mobileMenuRef.current,
+        {
+          height: "100vh",
+          opacity: 1,
+          duration: 0.5,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+    }
+  }, []);
+
+  // Control the timeline on isShowMobileMenu changes
+  useEffect(() => {
+    if (mobileMenuTl.current) {
       if (isShowMobileMenu) {
-        tl.play();
+        mobileMenuTl.current.play();
       } else {
-        tl.reverse();
+        mobileMenuTl.current.reverse();
       }
     }
   }, [isShowMobileMenu]);
@@ -56,7 +71,6 @@ const Navbar = () => {
     } else {
       document.body.classList.remove("overflow-hidden");
     }
-
     return () => {
       document.body.classList.remove("overflow-hidden");
     };
@@ -69,7 +83,6 @@ const Navbar = () => {
         setIsShowMobileMenu(false);
       }
     };
-
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
@@ -78,53 +91,47 @@ const Navbar = () => {
 
   return (
     <>
-      {!isShowMobileMenu && (
-        <div
-          id="homepage-navbar"
-          className="w-full z-10 px-[20px] md:px-[30px] desktop:px-[48px] h-[42px] bg-[#000000]/30 flex justify-between items-center fixed top-[0px]"
-        >
-          <Link href="/">
-            <img src="/logo.svg" className="w-[70px] invert" alt="Logo" />
-          </Link>
+      <div
+        id="homepage-navbar"
+        className="w-full z-10 px-[20px] md:px-[30px] desktop:px-[48px] h-[42px] bg-[#000000]/30 flex justify-between items-center fixed top-0"
+      >
+        <Link href="/" id="logo">
+          <img src="/logo.svg" className="w-[70px] invert" alt="Logo" />
+        </Link>
 
-          <div className="hidden md:flex text-white">
-            <Link href={"/"}>SPACE ONE</Link>
-            <Link href={"/"} className="ml-[40px]">
-              SPACE LOUNGE
-            </Link>
-            <Link href={"/"} className="ml-[40px]">
-              SPACE LAUNDROMAT{" "}
-            </Link>
-          </div>
-          <div></div>
-          <div
-            className="md:hidden cursor-pointer"
-            onClick={() => setIsShowMobileMenu(!isShowMobileMenu)}
-          >
-            <img
-              src="/images/hamburger.svg"
-              className="w-[18px]"
-              alt="Open Menu"
-            />
-          </div>
+        <div className="hidden md:flex text-white">
+          <Link href={"/"}>SPACE ONE</Link>
+          <Link href={"/"} className="ml-[40px]">
+            SPACE LOUNGE
+          </Link>
+          <Link href={"/"} className="ml-[40px]">
+            SPACE LAUNDROMAT
+          </Link>
         </div>
-      )}
+        <div></div>
+        <div
+          id="hamburger"
+          className="md:hidden cursor-pointer"
+          onClick={() => setIsShowMobileMenu(!isShowMobileMenu)}
+        >
+          <img
+            src="/images/hamburger.svg"
+            className="w-[18px]"
+            alt="Open Menu"
+          />
+        </div>
+      </div>
 
       <div
         ref={mobileMenuRef}
-        className={`h-[0vh] w-full fixed hidden text-white bg-[#000000]/30 z-[100] backdrop-blur-2xl top-[0px] opacity-0 ${
-          isShowMobileMenu && "isShowMobileMenu"
-        }`}
+        className={`w-full fixed text-white bg-[#000000]/30 z-[100] backdrop-blur-2xl top-0 opacity-0 overflow-hidden`}
+        style={{ height: 0 }}
       >
         <div
           onClick={() => setIsShowMobileMenu(!isShowMobileMenu)}
-          className="h-[42px] flex  flex-col items-end justify-center mx-[20px] cursor-pointer"
+          className="h-[42px] flex flex-col items-end justify-center mx-[20px] cursor-pointer"
         >
-          <img
-            src="/images/cancel-icon-navbar.svg"
-            alt="Close Menu"
-            className=""
-          />
+          <img src="/images/cancel-icon-navbar.svg" alt="Close Menu" />
         </div>
         <div className="flex flex-col mx-[20px] mt-[101px]">
           <Link href={"/"} className="text-[26px] font-[400]">
