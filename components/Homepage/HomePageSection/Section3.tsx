ae717d020169ca9b41d09d89dc2f64b355b6a3c9
venkatesh k <venkateshk@venkateshs-MacBook-Pro.local>
2025-03-30 16:38:52 +0530

    responsive for the section 2 and 3

--- components/Homepage/HomePageSection/Section3.tsx
+++ components/Homepage/HomePageSection/Section3.tsx
@@ -6,7 +6,9 @@ const Section3 = () => {
     <div id="section1">
       <div className="min-h-[100vh] w-full desktop:min-h-[1024px] bg-[url('/homepageImages/section-3.png')]  bg-cover bg-center flex md:items-center">
         <div className="text-white mx-[20px] md:mx-[48px] w-full flex md:block items-center flex-col pt-[144px] md:pt-0">
-          <h1 className="text-[56px] font-[400]">Space Laundromat</h1>
+          <h1 className="text-[56px] font-[400]">
+            <span className="hidden md:inline">Space</span> Laundromat
+          </h1>
           <p className="text-[18px] font-[400] mt-[16px]">
             Level up your wash game.{" "}
           </p>
