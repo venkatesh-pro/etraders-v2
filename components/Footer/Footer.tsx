4bd76f885c3efd1edc4189b8ac0c2baa41287d46
venkatesh k <venkateshk@venkateshs-MacBook-Pro.local>
2025-03-31 16:57:05 +0530

    finished the footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,53 +3,75 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="px-[20px] md:px-[48px] py-[40px] text-white min-h-[385px] bg-black">
-      <p className="text-[#949192] text-[12px] font-[400]">
-        1. These are estimated weekly payments if you participate in Space
-        leasing. These estimates are subject to change and contingent on lease
-        approval and other factors. The calculator does not include taxes or
-        fees. Your applicable taxes and fees will be confirmed for you closer to
-        time of delivery. Early termination fees apply. Fees may be charged for
-        excessive wear plus any overage of damage. Late payments will incur a 5%
-        fee. Lease does not include maintenance or insurance. Complete terms
-        will be included in Modular Building Lease Agreement.
-      </p>
+    <div className="px-[20px] md:px-[48px] py-[40px] text-white min-h-[385px] md:h-full  bg-black">
+      <ul className="text-[#949192] text-[12px] font-[400] list-decimal ml-[12px]">
+        <li>
+          These are estimated weekly payments if you participate in Space
+          leasing. These estimates are subject to change and contingent on lease
+          approval and other factors. The calculator does not include taxes or
+          fees. Your applicable taxes and fees will be confirmed for you closer
+          to time of delivery. Early termination fees apply. Fees may be charged
+          for excessive wear plus any overage of damage. Late payments will
+          incur a 5% fee. Lease does not include maintenance or insurance.
+          Complete terms will be included in Modular Building Lease Agreement.
+        </li>
+      </ul>
 
-      <div className="grid mt-[60px] md:grid-cols-3 gap-10">
-        <div className="my-[30px]">
+      <div className="grid py-[60px] md:grid-cols-3 gap-10 desktop:h-[263px] ">
+        <div className="">
           <p className="text-[12px] text-[#949192] font-[400] tracking-letterSpacing1px">
             LINEUP
           </p>
           <div className="mt-[15px] ">
-            <p className="text-[18px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">Space One</p>
-            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">Space Lounge</p>
-            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">Space Laundromat</p>
+            <p className="text-[18px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              Space One
+            </p>
+            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              Space Lounge
+            </p>
+            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              Space Laundromat
+            </p>
           </div>
         </div>
-        <div className="my-[30px]">
+        <div className="">
           <p className="text-[12px] text-[#949192] font-[400] tracking-letterSpacing1px">
             COMPANY
           </p>
           <div className="mt-[15px] ">
-            <p className="text-[18px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">About</p>
-            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">Careers</p>
-            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">Newsroom</p>
-            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">Contact</p>
+            <p className="text-[18px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              About
+            </p>
+            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              Careers
+            </p>
+            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              Newsroom
+            </p>
+            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              Contact
+            </p>
           </div>
         </div>
-        <div className="my-[30px]">
+        <div className="">
           <p className="text-[12px] text-[#949192] font-[400] tracking-letterSpacing1px">
             CONNECT
           </p>
           <div className="mt-[15px] ">
-            <p className="text-[18px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">Instagram</p>
-            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">YouTube</p>
-            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">X</p>
+            <p className="text-[18px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              Instagram
+            </p>
+            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              YouTube
+            </p>
+            <p className="text-[18px] mt-[10px] font-[400] hover:opacity-[90%] transition-opacity duration-300 cursor-pointer">
+              X
+            </p>
           </div>
         </div>
       </div>
 
-      <div className="mt-[40px] flex justify-between md:items-center md:flex-row flex-col">
+      <div className="flex justify-between md:items-center md:flex-row flex-col">
         {/* for mobile */}
         <Link
           className="text-[12px] block text-[#949192] font-[400] md:hidden"
