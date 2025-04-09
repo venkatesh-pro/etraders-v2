752f4d20aebd02b74f7ec89e2dfb098fd6df8a39
venkatesh k <venkateshk@venkateshs-MacBook-Pro.local>
2025-04-09 17:55:38 +0530

    section 8 completed

---
+++ components/SpaceOne/Section8.tsx
@@ -0,0 +1,188 @@
+import React from "react";
+
+const Section8 = () => {
+  return (
+    <div className="w-full px-[20px] lg:px-[100px] lapL:px-[192px] pt-[96px] pb-[96px] md:pb-[48px] desktop:h-[1055px]">
+      <div className="w-full">
+        <h2 className=" text-soft font-[400]">
+          <span className="text-[40px]">Space One Tech Specs</span>
+          <span className="text-[28px] font-[500] relative -top-[4px]">
+            <sup>5</sup>
+          </span>
+        </h2>
+
+        <div>
+          {/* 1 */}
+          <hr className="border-[#e1e1e1] mt-[48px] mb-[40px]" />
+          <div className="flex flex-col lg:flex-row justify-between ">
+            <div className="w-full lg:w-[432px]">
+              <h2 className="font-[400] text-[12px] text-soft100 tracking-letterSpacing1px">
+                STRUCTURE
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">
+                Galvanized steel
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Integrated floor system base
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Relocation points
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">Metal fixtures</p>
+              <p className="text-soft mt-[10px] text-[18px]">Subfloor</p>
+            </div>
+
+            <div className="w-full lg:w-[432px]">
+              <h2 className="font-[400] text-[12px] mt-[60px] lg:mt-0 text-soft100 tracking-letterSpacing1px">
+                CLADDING
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">
+                Eco timber composite wall panelling
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Eco timber composite deck
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Vertical architectural steel wall panels
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Color-matched aluminum trim
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Corrugated roof panels
+              </p>
+            </div>
+
+            <div className="w-full lg:w-[432px] ">
+              <h2 className="font-[400] text-[12px] mt-[60px] lg:mt-0  text-soft100 tracking-letterSpacing1px">
+                TECHNOLOGY
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">
+                10-inch touch display
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                In-ceiling sound system
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Ambient lighting
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">Canopy lighting</p>
+              <p className="text-soft mt-[10px] text-[18px]">Barometer</p>
+            </div>
+          </div>
+          {/* 2 */}
+          <hr className="border-[#e1e1e1] mt-[96px] mb-[96px]" />
+          <div className="flex flex-col lg:flex-row justify-between ">
+            <div className="w-full lg:w-[432px]">
+              <h2 className="font-[400] text-[12px] text-soft100 tracking-letterSpacing1px">
+                INTERIOR
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">
+                Natural light oak composite floors
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Natural light oak textured wall panels
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Smooth-textured ceiling panels
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Solid black trim
+              </p>
+            </div>
+
+            <div className="w-full lg:w-[432px]">
+              <h2 className="font-[400] text-[12px] mt-[60px] lg:mt-0 text-soft100 tracking-letterSpacing1px">
+                MECHANICAL
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">
+                Non-combustible insulation
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Second-generation hardware
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Power connectivity
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Clipsal Iconic GPO
+              </p>
+            </div>
+
+            <div className="w-full lg:w-[432px] ">
+              <h2 className="font-[400] text-[12px] mt-[60px] lg:mt-0  text-soft100 tracking-letterSpacing1px">
+                GLASS
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">Sliding door</p>
+              <p className="text-soft mt-[10px] text-[18px]">Picture window</p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Black powder coating
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">Aluminium frame</p>
+            </div>
+          </div>
+          {/* 3 */}
+          <hr className="border-[#e1e1e1] mt-[96px] mb-[96px]" />
+          <div className="flex flex-col lg:flex-row justify-between ">
+            <div className="w-full lg:w-[432px]">
+              <h2 className="font-[400] text-[12px] text-soft100 tracking-letterSpacing1px">
+                KITCHEN
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">
+                Natural light oak cabinets
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Two burner electric cooktop
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Stone splash back
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Under mount sink
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Solid black faucet
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">Storage shelves</p>
+            </div>
+
+            <div className="w-full lg:w-[432px]">
+              <h2 className="font-[400] text-[12px] mt-[60px] lg:mt-0 text-soft100 tracking-letterSpacing1px">
+                BATHROOM
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">Shower</p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Textured porcelain tiles
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Wall-mounted vanity
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">Round basin</p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Panoramic mirror
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">Water closet</p>
+              <p className="text-soft mt-[10px] text-[18px]">Towel rail</p>
+              <p className="text-soft mt-[10px] text-[18px]">Robe hook</p>
+            </div>
+
+            <div className="w-full lg:w-[432px] ">
+              <h2 className="font-[400] text-[12px] mt-[60px] lg:mt-0  text-soft100 tracking-letterSpacing1px">
+                ENERGY
+              </h2>
+              <p className="text-soft mt-[20px] text-[18px]">
+                Solar panel roof
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">
+                Tesla Powerwall 3
+              </p>
+              <p className="text-soft mt-[10px] text-[18px]">Power cabinet</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section8;
